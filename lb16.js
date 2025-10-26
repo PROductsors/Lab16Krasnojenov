d53e7fbf80f1765390c86bdc80dd2f34d5d24d1d
PROductsors <kostik.kbs@gmail.com>
2025-10-26 22:23:46 +0200

    Add files via upload

---
+++ lb16.js
@@ -0,0 +1,17 @@
+let name = "Костя";
+let age = 19;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+let person = {
+    name: name,
+    age: age
+};
+
+let hobbies = ["ігри", "музика", "код"];
+
+console.log(person);
+console.log(hobbies);
